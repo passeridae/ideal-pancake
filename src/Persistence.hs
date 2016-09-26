dd520645f64549664384987701f8dbcc4ba889c7
Andrew <a@ndrewsemler.com>
2016-09-26 11:15:57 +1000

    added getUser function

--- src/Persistence.hs
+++ src/Persistence.hs
@@ -1,1 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Persistence where
+
+import Database.PostgreSQL.Simple
+import qualified Data.ByteString.Char8     as BSC
+
+import Types
+
+getUser :: BSC.ByteString -> Name -> IO User
+getUser cs (Name name) = do
+  conn <- connectPostgreSQL cs
+  [(un,ui)] <- query conn "select * from user where name = ?" (Only name)
+  return $ User (Name un) (InternalId ui)
