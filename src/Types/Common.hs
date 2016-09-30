7241b10b1b2418877ac9e3cdce03c8a6ae5bbf25
Oswyn Brent <obrent@atlassian.com>
2016-09-30 15:31:59 +1000

    Cleanup + refactor types, improve docs rendering, simplify accessor functions, add addCopy to inmemory store

---
+++ src/Types/Common.hs
@@ -0,0 +1,61 @@
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE DuplicateRecordFields      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE RecordWildCards            #-}
+
+module Types.Common where
+
+import           Control.Monad
+import           Data.Aeson
+import           Data.Aeson.Casing
+import           Data.Aeson.Types
+import           Data.String                          hiding (fromString)
+import           Data.Text                            (Text)
+import           Data.UUID
+import           Data.UUID.V4
+import           Database.PostgreSQL.Simple.FromField hiding (name)
+import           GHC.Generics
+import           Servant.Docs
+import           System.IO.Unsafe
+
+--------------------------------------------------------------------------------
+
+-- Aeson util
+
+defaultAeson :: Options
+defaultAeson = (aesonDrop 0 snakeCase){omitNothingFields = True}
+
+--------------------------------------------------------------------------------
+
+-- Internal Identifiers (UUID v4)
+
+newtype InternalId = InternalId
+  { unInternalId :: UUID
+  } deriving (FromField, Generic, Eq, Ord, Show)
+
+instance ToJSON InternalId where
+  toJSON InternalId{..} = String (toText unInternalId)
+
+instance FromJSON InternalId where
+  parseJSON = withText "InternalId" $ \x ->
+    case fromText x of
+      Nothing -> fail "Invalid UUID"
+      Just u -> return $ InternalId u
+
+instance ToSample InternalId where
+  toSamples _ = do
+    let ids = unsafePerformIO $ replicateM 10 nextRandom--singleSample (InternalId (fromJust $ fromString "0fac788a-51bb-453e-a14a-61d70df8781d"))
+    samples (map InternalId ids)
+
+--------------------------------------------------------------------------------
+
+-- Name
+
+newtype Name = Name Text
+  deriving (FromField, Generic, Eq, Ord, Show, IsString, FromJSON, ToJSON)
+
+instance ToSample Name where
+  toSamples _ = samples $ map Name ["Oswyn Brent", "Emily Olorin", "Tristram Healy", "Andrew Semler"]
+
+--------------------------------------------------------------------------------
