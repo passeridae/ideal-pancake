fc9c3599c3458b92337fef89af38f2e88b0c1e9a
Andrew <a@ndrewsemler.com>
2016-10-17 20:01:03 +1100

    changed Nor to Not in db.sql

--- database/db.sql
+++ database/db.sql
@@ -1,4 +1,4 @@
 CREATE TABLE IF NOT EXISTS users (name text NOT NULL, internalId uuid PRIMARY KEY);
 CREATE TABLE IF NOT EXISTS books (isbn text PRIMARY KEY, title text NOT NULL, authors text[] NOT NULL, publishers text[] NOT NULL, yearOfPublication timestamp NOT NULL );
 CREATE TABLE IF NOT EXISTS copies (copyId uuid PRIMARY KEY, copyOf REFERENCES books (isbn) NOT NULL,  copyNotes text);
-CREATE TABLE IF NOT EXISTS rentals (rentalId uuid PRIMARY KEY, copyId REFERENCES copies (copyId), userId REFERENCES users (internalId), returnDate timestamp NOR NULL); 
+CREATE TABLE IF NOT EXISTS rentals (rentalId uuid PRIMARY KEY, copyId REFERENCES copies (copyId), userId REFERENCES users (internalId), returnDate timestamp NOT NULL); 
