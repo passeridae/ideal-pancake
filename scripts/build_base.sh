e36300f8bf2bee9ec26e4917ecbed8fc1dd38ef5
Andrew <a@ndrewsemler.com>
2016-10-23 15:30:39 +1100

    linted + added retry to db conn + updated build scripts

--- scripts/build_base.sh
+++ scripts/build_base.sh
@@ -1,5 +1,4 @@
 #! /bin/bash -e
 
-export GIT_REV=$(git describe --always)
 TAG=oswynb/ideal-pancake
-docker build --rm=true --tag=$TAG-base:${GIT_REV} -f BaseDockerfile .
+docker build --rm=true --tag=$TAG-base:latest -f BaseDockerfile .
